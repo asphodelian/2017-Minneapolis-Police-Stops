9b89a60a2007ab969c01e27e357b5ec459306937
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-09-25 15:27:30 -0700

    Read in dataset, looked at summary and dimensions, viewed it and so there were lots of empty cells. had to only omit the ones with nothing in the citation column, thus making new dataset called mlp. checked dimensions and filled in the remaining empty cells with unknown

--- Coding/Stops V1.R
+++ Coding/Stops V1.R
@@ -8,3 +8,19 @@ library(readr)
 library(readxl)
 library(tidyr)
 
+###########
+# Dataset #
+###########
+
+stop <- read.csv("D:/Coding/R Storage/M748/Project/Data/MplsStops.csv")
+summary(stop)
+dim(stop)
+
+# NA omission
+mlp <- stop[!(is.na(stop$citationIssued) | stop$citationIssued == ""), ]
+dim(mlp)
+
+# write-in NA
+mlp[mlp == ''] <- "Unknown"
+
+
