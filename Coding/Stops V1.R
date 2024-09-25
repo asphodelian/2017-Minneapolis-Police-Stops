8d76ed33b0f82a1f3bf370d4a9cfd8b444909dce
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-09-25 15:25:32 -0700

    Added necessary libraries

--- Coding/Stops V1.R
+++ Coding/Stops V1.R
@@ -8,36 +8,3 @@ library(readr)
 library(readxl)
 library(tidyr)
 
-###########
-# Dataset #
-###########
-
-stop <- read.csv("D:/Coding/R Storage/M748/Project/Data/MplsStops.csv")
-summary(stop)
-dim(stop)
-
-# NA omission
-mlp <- stop[!(is.na(stop$citationIssued) | stop$citationIssued == ""), ]
-dim(mlp)
-
-# write-in NA
-mlp[mlp == ''] <- "Unknown"
-
-# vars
-num <- mlp$rownames
-ID <- mlp$idNum
-date <- as.Date(mlp$date)
-problem <- as.numeric(mlp$problem)
-mdc <- as.numeric(mlp$MDC)
-citeIssue <- as.numeric(mlp$citationIssued)
-personSearch <- as.numeric(mlp$personSearch)
-vehicleSearch <- as.numeric(mlp$vehicleSearch)
-preRace <- as.numeric(mlp$preRace)
-gender <- as.numeric(mlp$gender)
-lat <- mlp$lat
-long <- mlp$long
-precinct <- mlp$policePrecinct
-neighbor <- as.numeric(mlp$neighborhood)
-
-
-
