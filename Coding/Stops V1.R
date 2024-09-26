dbe4275f57b5a0c7ef33766edc42b2dfa937c658
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-09-25 22:07:06 -0700

    Removed MDC column, did tables of the columns for count

--- Coding/Stops V1.R
+++ Coding/Stops V1.R
@@ -27,11 +27,24 @@ mlp[mlp == ''] <- "Unknown"
 num <- mlp$rownames
 lat <- mlp$lat
 long <- mlp$long
+mdc <- mlp$MDC
 
 # column removal
-police <- subset(mlp, select = -c(num,lat,long))
+police <- subset(mlp, select = -c(mdc,num,lat,long))
 
 # yes/no --> 1/0
 police$citationIssued <- ifelse(police$citationIssued == "YES",1,0)
 police$personSearch <- ifelse(police$personSearch == "YES",1,0)
 police$vehicleSearch <- ifelse(police$vehicleSearch == "YES",1,0)
+
+# counts
+table(as.Date(police$date))
+table(police$problem)
+table(police$citationIssued)
+table(police$personSearch)
+table(police$vehicleSearch)
+table(police$preRace)
+table(police$race)
+table(police$gender)
+table(police$policePrecinct)
+table(police$neighborhood)
