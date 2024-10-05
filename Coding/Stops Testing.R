53f51c53ce95469879d3a72712e8f3babb158755
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-10-04 21:11:44 -0700

    Tossed ggplot2 color palette into here for safekeeping

--- Coding/Stops Testing.R
+++ Coding/Stops Testing.R
@@ -9,3 +9,4 @@ dim(stop)
 problem <- stop$problem
 summary(as.numeric(problem))
 
+# scale_fill_manual(values = c("firebrick4", "darkorange2", "goldenrod1", "seagreen4", "dodgerblue3", "slateblue2", "darkviolet") ) +
