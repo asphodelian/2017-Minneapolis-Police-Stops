b38e809d4a5228a849f0395a0762e6fcf348e140
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-11-17 03:20:02 -0800

    Called police dataset and checked its specs

--- Coding/Stops V2.R
+++ Coding/Stops V2.R
@@ -8,3 +8,12 @@ library(psych)
 library(readr)
 library(readxl)
 library(tidyr)
+
+###########
+# Dataset #
+###########
+
+police <- read.csv("D:/Coding/R Storage/M748/Project/Data/police.csv", stringsAsFactors = TRUE)
+summary(police)
+summary(is.na(police))
+dim(police)
