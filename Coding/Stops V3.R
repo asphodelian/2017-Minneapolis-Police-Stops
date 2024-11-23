3665681b2e8ee6ebcd8a9680bae45d4613fe8936
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-11-22 17:51:50 -0800

    Added data edits & separated policeDate into one with problem -> (1,0) and one kept the same

--- Coding/Stops V3.R
+++ Coding/Stops V3.R
@@ -28,3 +28,23 @@ police <- read.csv("D:/Coding/R Storage/M748/Project/Data/police.csv", stringsAs
 summary(police)
 summary(is.na(police))
 dim(police)
+
+################
+# Data Editing #
+################
+
+# Splitting the Date 
+police$date <- as.Date(police$date)
+policeDate <- transform(police, 
+                     date = format(date, "%d"), 
+                     month = format(date, "%m"), 
+                     year = format(date, "%Y"))
+
+polDate <- subset(policeDate, select = -c(date, year))
+# problem into 1,0
+policeDate$problem <- ifelse(polDate$problem == "traffic",1,0)
+
+
+# removing columns for PCA
+numPol <- subset(policeDate, select = -c(X, idNum, preRace, race, year, date, neighborhood, gender))
+
