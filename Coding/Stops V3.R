df4252aff53db0d533978beb09542e5ea05f2a0c
Gabrielle Salamanca <knightcallie73@hotmail.com>
2024-11-22 17:53:06 -0800

    Added PCA

--- Coding/Stops V3.R
+++ Coding/Stops V3.R
@@ -48,3 +48,44 @@ policeDate$problem <- ifelse(polDate$problem == "traffic",1,0)
 # removing columns for PCA
 numPol <- subset(policeDate, select = -c(X, idNum, preRace, race, year, date, neighborhood, gender))
 
+#######
+# PCA #
+#######
+
+# normalizing
+numPol$month <- as.numeric(numPol$month)
+normPol <- scale(numPol)
+
+# PCA
+polCA <- princomp(normPol)
+summary(polCA)
+polCA$loadings 
+
+# visualize
+fviz_eig(polCA, addlabels = TRUE)
+fviz_pca_var(polCA, col.var = "mediumslateblue")
+fviz_cos2(polCA, choice = "var", axes = 1:2)
+fviz_pca_var(polCA, col.var = "cos2",
+             gradient.cols = c("mediumslateblue", "orange", "green"),
+             repel = TRUE)
+
+# prcomp
+result <- prcomp(numPol, scale. = TRUE)
+summary(result)
+var <- result$sdev^2
+ratio <- var/sum(var)
+scree <- data.frame(
+  Principal_Component = 1:length(ratio),
+  Variance_Explained = ratio
+)
+
+ggplot(scree, aes(x = Principal_Component, y = Variance_Explained)) +
+  geom_line(group = 1) +
+  geom_point() +
+  scale_x_continuous(breaks = 1:nrow(scree)) +
+  labs(
+    title = "Scree Plot",
+    x = "Principal Component",
+    y = "Proportion of Variance Explained"
+  ) +
+  theme_minimal()
